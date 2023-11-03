45c7f2d2938aea2058320d29fb25c8189e7544ea
JackMac10 <macdonaldjackson0@gmail.com>
2023-11-03 12:06:00 -0700

    hello.js

---
+++ lighthouse-js-fundamentals/chorus-while.js
@@ -0,0 +1,17 @@
+let count = 100;
+while (count < 201) {
+    if (count % 3 && count % 4)
+  console.log("loopy lighthouse")
+
+    else if (count % 3)
+  console.log("loopy")
+
+    else if (count % 4)
+  console.log("lighthouse")
+
+    else
+  console.log(count);
+  
+  count++; 
+  }
+
